c5290bcb7bd431092f0c1ea676a471716dbb1711
JOSE ORDOÑEZ <restonjd@gmail.com>
2015-07-08 16:29:48 -0430

    evento y blog en index; funcional
    faltan detalles de css y responsive

--- cic/apps/homepage/views.py
+++ cic/apps/homepage/views.py
@@ -4,27 +4,28 @@ from django.shortcuts import render_to_response
 from .forms import *
 from .models import *
 from django.utils.timezone import now, timedelta
-from zinnia.models import Category
 from django.template import RequestContext
 from django.core.mail import send_mail
 from django.views.generic import CreateView
 from django.shortcuts import redirect
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404
+
 from cic.apps.calendarium.models import Event
+from zinnia.models import Entry
+from zinnia.models import Category
+
 import datetime
 import nltk
 
 
-def index(request):	
-#	entradas = Entry.objects.filter(status=2).order_by('-creation_date')
-#	entradas = entradas.exclude(start_publication__gte=datetime.date.today())
-#	entradas = entradas[:3]
-#	titulos = []
-#	for ent in entradas:
-#		quitar_html = nltk.clean_html(ent.content) 
-#		ent.content =  quitar_html[:200]
-#		titulos.append(ent.title)	
+def index(request):
+	#entradas blog
+	entradas = Entry.objects.filter(status=2).order_by('-creation_date')
+	entradas = entradas.exclude(start_publication__gte=datetime.date.today())[:6]
+	for ent in entradas:
+		quitar_html = nltk.clean_html(ent.content)
+		ent.content = quitar_html[:100]
 	#eventos del calendario
 	if Event.objects.get_occurrences(now(), now() + timedelta(days=356), None):
 		nextEvent = True
@@ -33,12 +34,15 @@ def index(request):
 	textoshome = textosInicio.objects.get(pk=1)
 	imgslider = sliderInicio.objects.all()
 	videosinicio = videosInicio.objects.all()
-	ctx = {'textoshome':textoshome,'imgslider':imgslider,'videosinicio': videosinicio,'nextEvent': nextEvent,}	
+	ctx = {'textoshome': textoshome, 'imgslider': imgslider, 'videosinicio': videosinicio, 'nextEvent': nextEvent, 'entradas': entradas}
 	return render_to_response('homepage/index.html', ctx, context_instance=RequestContext(request))
+
+
 def recursos(request):
 	categorias = Category.objects.all()
-	ctx = {'categorias':categorias}	
+	ctx = {'categorias': categorias}
 	return render_to_response('homepage/recursos.html', ctx, context_instance=RequestContext(request))
+
 
 def aboutus(request):
 	title = "Quienes Somos"
@@ -47,47 +51,51 @@ def aboutus(request):
 	except Exception:
 		quienestext = None
 	try:
-		directores = miembros.objects.filter(tipo_de_miembro='directivo')	
+		directores = miembros.objects.filter(tipo_de_miembro='directivo')
 	except Exception:
-		directores = None		
+		directores = None
 	try:
-		representantes_internacionales = representantesInternacionales.objects.all()	
+		representantes_internacionales = representantesInternacionales.objects.all()
 	except Exception:
-		directores = None	
+		directores = None
 	try:
-		otrostextos = quienesSomosOtros.objects.get(pk=1)	
+		otrostextos = quienesSomosOtros.objects.get(pk=1)
 	except Exception:
-		otrostextos = None			
-	ctx = {'title':title, 'quienesSomos':quienestext, 'directores':directores,'otrostextos':otrostextos, 'representantes_internacionales':representantes_internacionales}	
-	return render_to_response('homepage/quienessomos.html',ctx, context_instance=RequestContext(request))
+		otrostextos = None
+	ctx = {'title': title, 'quienesSomos': quienestext, 'directores': directores, 'otrostextos': otrostextos, 'representantes_internacionales': representantes_internacionales}
+	return render_to_response('homepage/quienessomos.html', ctx, context_instance=RequestContext(request))
+
 
 def serviciosCursos(request):
 	try:
 		servicios_textos = servicios.objects.get(pk=1)
 	except Exception:
-		servicios_textos = None	
+		servicios_textos = None
 	try:
 		cursos_lista = cursoslista.objects.all()
 	except Exception:
-		cursos_lista = None		
-	ctx = {'servicios_textos':servicios_textos, 'cursoscursos':cursos_lista,}	
-	return render_to_response('homepage/servicios.html',ctx, context_instance=RequestContext(request))
+		cursos_lista = None
+	ctx = {'servicios_textos': servicios_textos, 'cursoscursos': cursos_lista}
+	return render_to_response('homepage/servicios.html', ctx, context_instance=RequestContext(request))
+
 
 def cursodetalle(request, slugcurso):
+	success = False
 	if request.method == 'POST':
 		form = solicitudForm(request.POST)
 		if form.is_valid():
 			success = True
 			cd = form.cleaned_data
 	else:
-		form = solicitudForm()		
+		form = solicitudForm()
 	curso = get_object_or_404(cursoslista, slugcurso=slugcurso)
-	ctx = {'curso':curso,'form':form}
+	ctx = {'curso': curso, 'form': form}
 	return render_to_response('homepage/cursodetalle.html', ctx, context_instance=RequestContext(request))
 
 
 def directorio(request):
 	return render_to_response('homepage/directorio.html', context_instance=RequestContext(request))
+
 
 def paises(request):
 	title = "directoriocic"
@@ -96,23 +104,25 @@ def paises(request):
 	except Exception:
 		quienestextos = None
 	try:
-		paisesdelacic = paisesCic.objects.all()	
+		paisesdelacic = paisesCic.objects.all()
 	except Exception:
-		paisesdelacic = None	
-	ctx = {'title':title,'quienesSomos':quienestextos,'paisescic':paisesdelacic}			
+		paisesdelacic = None
+	ctx = {'title': title, 'quienesSomos': quienestextos, 'paisescic': paisesdelacic}
 	return render_to_response('homepage/paises.html', ctx, context_instance=RequestContext(request))
+
 
 def codigoetica(request):
 	try:
 		quienestextos = seccionesQuienesSomos.objects.get(pk=1)
 	except Exception:
-		quienestextos = None	
-	ctx = {'quienesSomos':quienestextos}
-	return render_to_response('homepage/codigoetica.html',ctx, context_instance=RequestContext(request))
+		quienestextos = None
+	ctx = {'quienesSomos': quienestextos}
+	return render_to_response('homepage/codigoetica.html', ctx, context_instance=RequestContext(request))
+
 
 def certificaciones(request):
 	try:
-		certificacionesintro = certificacionesIntroduccion.objects.get(pk=1)	
+		certificacionesintro = certificacionesIntroduccion.objects.get(pk=1)
 	except Exception:
 		certificacionesintro = None
 	try:
@@ -122,45 +132,50 @@ def certificaciones(request):
 	try:
 		especializaciones_lista = certificacionesLista.objects.filter(tipo="especializacion")
 	except Exception:
-		especializaciones_lista = None			
-	ctx = {'certificaciones':certificacionesintro,'certificaciones_lista':certificaciones_lista, 'especializaciones_lista':especializaciones_lista}			
+		especializaciones_lista = None
+	ctx = {'certificaciones': certificacionesintro, 'certificaciones_lista': certificaciones_lista, 'especializaciones_lista': especializaciones_lista}
 	return render_to_response('homepage/certificaciones.html', ctx, context_instance=RequestContext(request))
 
+
 def certificaciondetalle(request, slug):
+	success = False
 	if request.method == 'POST':
 		form = solicitudForm(request.POST)
 		if form.is_valid():
 			success = True
 			cd = form.cleaned_data
 	else:
-		form = solicitudForm()		
+		form = solicitudForm()
 	certificacion = get_object_or_404(certificacionesLista, slug=slug)
-	ctx = {'certificacion':certificacion,'form':form}
+	ctx = {'certificacion': certificacion, 'form': form, 'success': success}
 	return render_to_response('homepage/certificaciondetalle.html', ctx, context_instance=RequestContext(request))
+
 
 def conferencias(request):
 	try:
 		serviciostxt = servicios.objects.get(pk=1)
 	except Exception:
-		serviciostxt = None	
+		serviciostxt = None
 	print serviciostxt.conferencias
 	try:
-		conferenciaslist= conferenciaslista.objects.all()
+		conferenciaslist = conferenciaslista.objects.all()
 	except Exception:
-		conferenciaslist = None			
-	ctx = {'servi':serviciostxt,'conferenciaslist':conferenciaslist}
+		conferenciaslist = None
+	ctx = {'servi': serviciostxt, 'conferenciaslist': conferenciaslist}
 	return render_to_response('homepage/conferencias.html', ctx, context_instance=RequestContext(request))
 
+
 def conferenciasdetalle(request, slugconf):
+	success = False
 	if request.method == 'POST':
 		form = solicitudForm(request.POST)
 		if form.is_valid():
 			success = True
 			cd = form.cleaned_data
 	else:
-		form = solicitudForm()			
+		form = solicitudForm()
 	conferencia = get_object_or_404(conferenciaslista, slugconf=slugconf)
-	ctx = {'conferencia':conferencia,'form':form}
+	ctx = {'conferencia': conferencia, 'form': form, 'success': success}
 	return render_to_response('homepage/conferenciadetalle.html', ctx, context_instance=RequestContext(request))
 
 
@@ -168,12 +183,14 @@ def asesorias(request):
 	try:
 		serviciostextos = servicios.objects.get(pk=1)
 	except Exception:
-		serviciostextos = None	
-	ctx = {'serviciostextos':serviciostextos}
+		serviciostextos = None
+	ctx = {'serviciostextos': serviciostextos}
 	return render_to_response('homepage/asesorias.html', ctx, context_instance=RequestContext(request))
+
 
 def registro(request):
 	return render_to_response('homepage/registro.html', context_instance=RequestContext(request))
+
 
 def registrocic(request):
 	success = False
@@ -181,12 +198,13 @@ def registrocic(request):
 		form = MemberForm(request.POST)
 		if form.is_valid():
 			success = True
-			cd = form.cleaned_data
+			#cd = form.cleaned_data
 			form.save()
 	else:
 		form = MemberForm()
-	ctx = {'form': form, 'success': success}	
-	return render_to_response('homepage/registrocic.html',ctx, context_instance=RequestContext(request))
+	ctx = {'form': form, 'success': success}
+	return render_to_response('homepage/registrocic.html', ctx, context_instance=RequestContext(request))
+
 
 def registroinvitado(request):
 	success = False
@@ -194,13 +212,14 @@ def registroinvitado(request):
 		form = MiembroInvitadoForm(request.POST)
 		if form.is_valid():
 			success = True
-			cd = form.cleaned_data
+			#cd = form.cleaned_data
 			form.save()
 	else:
 		form = MiembroInvitadoForm()
-	ctx = {'form': form, 'success': success}	
-	return render_to_response('homepage/registroinvitado.html',ctx, context_instance=RequestContext(request))
- 	
+	ctx = {'form': form, 'success': success}
+	return render_to_response('homepage/registroinvitado.html', ctx, context_instance=RequestContext(request))
+
+
 def contact(request):
 	success = False
 	if request.method == 'POST':
